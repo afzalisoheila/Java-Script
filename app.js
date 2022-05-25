b9b2dfca8055cd0245c8ce46998e43e7ce09a119
afzalisoheila <soheila.afzali@yahoo.com>
2022-05-25 11:18:29 +0200

    if and else if practice

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 alert("hello")
 console.log(document.getElementById("welcome"));
 
-function Sum(x, y){
-    return "totalSum" + (x + y)
-}
+// function Sum(x, y){
+//     return "totalSum" + (x + y)
+// }
 
-let result = Sum(4, 6)
-console.log(result)
+// let number1=4
+// let number2 = 6
+
+// let result = Sum(number1,number2 )
+// console.log(result)
+let month = 4
+if (month <= 3) {
+    console.log("spring");
+}
+else if (month >3 && month <=6) {
+    console.log("summer");
+}
+else if (month >6 && month <=9) {
+    console.log("autumn");
+}
+else if (month <=12) {
+    console.log("winter");
+} 
+let email =
+let pass=12
