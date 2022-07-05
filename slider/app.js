dc32477a906e1c694a505d18bda7c809def9b10f
afzalisoheila <soheila.afzali@yahoo.com>
2022-07-05 15:18:33 +0200

    slider -prev button in js

--- slider/app.js
+++ slider/app.js
@@ -1,32 +1,36 @@
 const images = document.querySelectorAll(".slider img")
+const indicators = document.querySelectorAll(".indicators span")
 
  images.forEach(element => {
    element.style.display= "none"
-    
+     
  });
-let i = 0
-images[i].style.display = "block"
+let index = 0
+images[index].style.display = "block"
+indicators[index].style.opacity = 100%
 document.querySelector(".next").addEventListener("click", () => {
-    images[i].style.display = "none"
-    i++
-    if (i >= images.length) {
-        i = 0; 
+    images[index].style.display = "none"
+    indicators[index].style.opacity = 40%
+    index++
+    if (index >= images.length ) {
+        index = 0; 
     }
 
-    images[i].style.display= "block"
+    images[index].style.display = "block"
+    indicators[index].style.opacity = "100%" 
     
 })
 
-let r = 0
-images[r].style.display = "block"
+let i = 0
+images[i].style.display = "block"
 document.querySelector(".prev").addEventListener("click", () => {
-    images[r].style.display = "none"
-    r--
-    if (r <= images.length) {
-        r = 0; 
+    images[i].style.display = "none"
+    i--
+    if (i <images.length) {
+        i = 0; 
     }
     
-    images[r].style.display= "block"
+    images[i].style.display= "block"
     
 })
 console.log(document.querySelectorAll(".slider img"));
